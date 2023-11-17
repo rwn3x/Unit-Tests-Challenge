de164d91481ff804753faa0836736e8d7f468f66
rwn3x <98042414+rwn3x@users.noreply.github.com>
2023-11-17 15:11:00 -0600

    Update mod13_rwn3x.py

--- mod13_rwn3x.py
+++ mod13_rwn3x.py
@@ -1,1 +1,9 @@
+#symbol: capitalized, 1-7 alpha characters
 
+#chart type: 1 numeric character, 1 or 2
+
+#time series: 1 numeric character, 1 - 4
+
+#start date: date type YYYY-MM-DD
+
+#end date: date type YYYY-MM-DD
